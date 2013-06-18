859be2523e18a06eeeaee87a6dae977fced984d9
patrickreed1994 <patrick.reed1994@gmail.com>
2013-06-18 01:45:46 -0300

    Update compare_rectime.py

--- compare_rectime.py
+++ compare_rectime.py
@@ -8,5 +8,3 @@ def update(total, a):
 		for number in range(len(a)):
 			print time.strftime("%H:%M:%S"), iter, rec(a[number])
 		time.sleep(10)
-	
-print update(10, ['aapl', 'adbe', 'driv', 'gff', 'goog', 'ibm', 'ioc', 'ixc', 'mstr', 'nls', 'ogzpy', 'sbr'])
